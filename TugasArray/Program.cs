a55f095963bea58becd7fc3e28fee4484d4604ae
linndand <linnda.p.n@students.amikom.ac.id>
2023-06-29 11:11:52 +0700

    TugasArray

---
+++ TugasArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasArray
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrNo = {"1.", "2.", "3." };
+            string[] arrNim = { "1234", "1235", "1235" };
+            string[] arrMhs = { "erick", "wely", "sony" };
+            int[] arrNilai = { 75, 80, 95 };
+
+            Console.WriteLine("No.\tNIM\tNama\tNilai");
+            Console.WriteLine("===========================");
+
+            //menampilkan semua elemen
+            for (int i = 0; i < arrMhs.Length; i++)
+            {
+                Console.WriteLine($"{arrNo[i]}\t{arrNim[i]}\t{arrMhs[i]}\t{arrNilai[i]}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
